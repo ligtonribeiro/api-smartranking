2d83396403a4a00d78ac73c7e654dfdce41fd4fe
Wellington Menezes <ligton.ribeiro@gmail.com>
2021-11-27 21:16:26 -0300

    Implementado todos os métodos responsáveis pelo o CRUD da API

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
+import { Jogador } from './interfaces/jogador.interface';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -8,6 +9,22 @@ export class JogadoresController {
 
   @Post()
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
-    this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
+    await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
+  }
+
+  @Get()
+  async consultarJogadores(
+    @Query('email') email: string,
+  ): Promise<Jogador[] | Jogador> {
+    if (email) {
+      return this.jogadoresService.consultarJogadoresPorEmail(email);
+    } else {
+      return this.jogadoresService.consultarTodosJogadores();
+    }
+  }
+
+  @Delete()
+  async deletarJogador(@Query('email') email: string): Promise<void> {
+    this.jogadoresService.deletarJogador(email);
   }
 }
